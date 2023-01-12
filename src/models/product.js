22dc22428302d016f743eea4db3ebafdebcd874b
Simon Dahal <simonsd054@gmail.com>
2023-01-12 12:24:52 +1100

    Add validation in Product Schema

--- src/models/product.js
+++ src/models/product.js
@@ -6,9 +6,26 @@ const ReviewSchema = new mongoose.Schema({
 })
 
 const ProductSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    price: Number,
+    title: {
+        type: String,
+        required: true,
+        minLength: 5,
+    },
+    description: {
+        type: String,
+        validate: {
+            validator: (value) => {
+                //Suppose, decsription cannot contain the word "hello"
+                return !value.includes("hello")
+            },
+            message: "Description cannot contain the word 'hello'",
+        },
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, "No Negative Price"],
+    },
     stock: Number,
     reviews: [ReviewSchema],
 })
